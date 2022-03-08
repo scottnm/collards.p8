f8494dae4b81badeba552ee9cc226812f8684f4d
scott munro <scottnmunro@gmail.com>
2022-03-07 19:48:59 -0800

    stub p8 cart and code file

---
+++ collards.p8
@@ -0,0 +1,6 @@
+pico-8 cartridge // http://www.pico-8.com
+version 29
+__lua__
+
+-- run the main game
+#include main.lua
---
+++ main.lua
@@ -0,0 +1,14 @@
+-- main file for collards.p8
+
+function _init()
+    -- noop
+end
+
+function _update()
+    -- noop
+end
+
+function _draw()
+    cls(3)
+    print("hello world", 64, 64, 7)
+end
