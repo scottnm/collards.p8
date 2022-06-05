7e0820cedab3be35e2dc78983f6ea2a7dfb40f54
scott munro <scottnmunro@gmail.com>
2022-06-04 23:27:41 -0700

    added some letter bob. looking pretty good!

--- pregame.lua
+++ pregame.lua
@@ -12,6 +12,18 @@ function _init_title_screen()
         g = 198,
         e = 200,
         p = 202 }
+
+    g_title = {
+        g_title_letters.d,
+        g_title_letters.i,
+        g_title_letters.g,
+        g_title_letters.d,
+        g_title_letters.e,
+        g_title_letters.e,
+        g_title_letters.p,
+    }
+
+    g_letter_motion = 0
 end
 
 function _update_title_screen(input)
@@ -19,6 +31,7 @@ function _update_title_screen(input)
     if input.any_change then
         set_phase(GamePhase.MainGame)
     end
+    g_letter_motion += 1
 end
 
 function modify_player_palette()
@@ -28,13 +41,12 @@ end
 function draw_title_text(color, x, y)
     pal(Colors.Maroon, color)
     local scale = 15
-    sspr_centered(g_title_letters.d, x + 0*scale, y, 2, 2, 1, false, false)
-    sspr_centered(g_title_letters.i, x + 1*scale, y, 2, 2, 1, false, false)
-    sspr_centered(g_title_letters.g, x + 2*scale, y, 2, 2, 1, false, false)
-    sspr_centered(g_title_letters.d, x + 3*scale, y, 2, 2, 1, false, false)
-    sspr_centered(g_title_letters.e, x + 4*scale, y, 2, 2, 1, false, false)
-    sspr_centered(g_title_letters.e, x + 5*scale, y, 2, 2, 1, false, false)
-    sspr_centered(g_title_letters.p, x + 6*scale, y, 2, 2, 1, false, false)
+    for i=0,(count(g_title)-1) do
+        local letter_motion = g_letter_motion + i*4
+        local bob = sin(letter_motion%30/60) * 3
+        printh(letter_motion.." "..bob)
+        sspr_centered(g_title[i+1], x + i*scale, y + bob, 2, 2, 1, false, false)
+    end
 end
 
 function _draw_title_screen()
@@ -45,6 +57,6 @@ function _draw_title_screen()
     draw_anim(g_player, sprite_pos(g_player))
     pal()
 
-    draw_title_text(Colors.Tan, 9, 9)
-    draw_title_text(Colors.Maroon, 10, 10)
+    draw_title_text(Colors.Tan, 21, 31)
+    draw_title_text(Colors.Maroon, 22, 32)
 end
